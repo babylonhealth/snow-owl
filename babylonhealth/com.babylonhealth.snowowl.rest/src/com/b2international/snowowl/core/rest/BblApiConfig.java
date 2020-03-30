223ede27d553d7e4c3e33e845fd1e07930cd03a3
ivelina.yordanova <ivelina.yordanova@babylonhealth.com>
2020-03-30 18:53:09 +0100

    add MonitoringRestService

--- babylonhealth/com.babylonhealth.snowowl.rest/src/com/b2international/snowowl/core/rest/BblApiConfig.java
+++ babylonhealth/com.babylonhealth.snowowl.rest/src/com/b2international/snowowl/core/rest/BblApiConfig.java
@@ -1,18 +1,12 @@
 package com.b2international.snowowl.core.rest;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 import springfox.documentation.spring.web.plugins.Docket;
 
-/**
- * The Spring configuration class for Snow Owl's FHIR REST API.
- * 
- * @since 7.5
- */
+
 @Configuration
-@ComponentScan("com.b2international.snowowl.core.rest")
 public class BblApiConfig extends BaseApiConfig {
 
 	@Override
